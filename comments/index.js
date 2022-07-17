59c27d124811480c345cd9a83dabdce141c410b7
Sunday Chukwuebuka <ikesundayshield@gmail.com>
2022-07-17 14:46:37 +0100

    recieving events

--- comments/index.js
+++ comments/index.js
@@ -14,7 +14,7 @@ app.get('/posts/:id/comments', (req, res) => {
     res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
     const commentId = randomBytes(4).toString('hex');
     const { content } = req.body;
 
@@ -22,7 +22,7 @@ app.post('/posts/:id/comments', (req, res) => {
     comments.push({ id: commentId, content });
     commentsByPostId[req.params.id] = comments;
 
-    axios.post('http:localhost:4005/events', {
+    await axios.post('http://localhost:4005/events', {
         type: 'CommentCreated',
         data: {
             id: commentId,
@@ -32,6 +32,12 @@ app.post('/posts/:id/comments', (req, res) => {
     })
 
     res.status(201).send(comments);
+});
+
+app.post('/events', (req, res) => {
+    console.log('Event Recieved', req.body.type);
+
+    res.send({});
 });
 
 app.listen(4001, () => {
