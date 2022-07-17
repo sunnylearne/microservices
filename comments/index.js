10e73772e8652a0a3a1f0270aba8369e24909e7a
Sunday Chukwuebuka <ikesundayshield@gmail.com>
2022-07-17 14:23:05 +0100

    emiting comment creation events

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -20,6 +21,15 @@ app.post('/posts/:id/comments', (req, res) => {
     const comments = commentsByPostId[req.params.id] || [];
     comments.push({ id: commentId, content });
     commentsByPostId[req.params.id] = comments;
+
+    axios.post('http:localhost:4005/events', {
+        type: 'CommentCreated',
+        data: {
+            id: commentId,
+            content,
+            postId: req.params.id
+        }
+    })
 
     res.status(201).send(comments);
 });
